2c452c3e07a85bab41beaa4827e51067aecb076a
jf-arce <josefranciscoarce21@gmail.com>
2025-04-16 12:23:39 -0300

    feat: update CreateBarberDto to support optional Bio and add LanguageIds and SkillIds properties

--- src/Application/Dtos/Barbers/CreateBarberDto.cs
+++ src/Application/Dtos/Barbers/CreateBarberDto.cs
@@ -4,5 +4,7 @@ namespace Application.Dtos.Barbers;
 
 public class CreateBarberDto : RegisterDto
 {
-    public string Bio { get; set; } = string.Empty;
+    public string? Bio { get; set; }
+    public List<int> LanguageIds { get; set; } = null!;
+    public List<int> SkillIds { get; set; } = null!;
 }
