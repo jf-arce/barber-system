7c80176de27a3dea712496f9cce137d9af8fc2bb
jf-arce <josefranciscoarce21@gmail.com>
2025-04-19 17:20:53 -0300

    feat(navbar): add MainNavbar component with navigation links and styling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,27 @@
-import Link from "next/link";
+import { MainContainer } from "@/components/containers/MainContainer";
+import { MainNavbar } from "@/components/MainNavbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Barber</h1>
+      <MainNavbar />
+      <div className="relative max-h-[500px] overflow-hidden">
+        <Image 
+          src="/images/image-main.jpg"
+          alt="Barber"
+          width={1920}
+          height={1080}
+          className="object-cover object-top w-full h-full"  
+        />  
+      </div>	
+      <MainContainer>
+        <h1 className="text-center text-5xl font-bold p-10">No solo cortamos pelo, creamos estilo</h1>
+      </MainContainer>
+      {/* <h1>Barber</h1>
       <Link href="/auth/login">Login</Link>
       <br />
-      <Link href="/auth/register">Register</Link>
+      <Link href="/auth/register">Register</Link> */}
     </div>
   );
 }
