c552f1b632c14830c1c1862f0cec92bee65e4472
jf-arce <josefranciscoarce21@gmail.com>
2025-08-24 14:09:54 -0300

    refactor: simplify MainContainer props and add PageContainer component

--- frontend/src/core/layouts/PageContainer.tsx
+++ frontend/src/core/layouts/PageContainer.tsx
@@ -1,9 +1,9 @@
-interface MainContainerProps {
+interface PageContainerProps {
 	children: React.ReactNode;
 	className?: string;
 }
 
-export const MainContainer = ({ children, className }: MainContainerProps) => (
+export const PageContainer = ({ children, className }: PageContainerProps) => (
 	<div
 		className={`w-full px-4 sm:px-8 xl:max-w-[80rem] 2xl:max-w-[96rem] mx-auto ${className || ""}`}
 	>
